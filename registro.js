4138108a02a6550d131d40376c4b5a0ab28b2c12
manuelmesa1 <manuelmesa12@gmail.com>
2021-10-05 18:56:31 -0500

    MM_10.5.21_7PM

---
+++ registro.js
@@ -0,0 +1,16 @@
+let registros = [];
+
+function obtenerusername(arreglo){
+    
+}
+function filtrarCorreo(arreglo){
+        
+} 
+function agregarRegistro(){
+
+}
+
+module.exports.registros = registros;
+module.exports.obtenerusername = obtenerusername;
+module.exports.filtrarCorreo = filtrarCorreo;
+module.exports.agregarRegistro = agregarRegistro;
